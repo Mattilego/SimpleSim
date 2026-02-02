05d0a97f01b4fc3f444129de21dc0467f9b3eef4
Mattias <mattiasstrandberg4@gmail.com>
2026-02-02 23:17:51 +0100

    Removed omments from json for easier parsing, started work on the site ui

--- Server.js
+++ Server.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+// Specific route for index.html to serve GUI.html
+app.get('/index.html', (req, res) => {
+	res.sendFile(path.join(__dirname, 'localGUI', 'GUI.html'));
+});
+
+app.get('/style.css', (req, res) => {
+	res.sendFile(path.join(__dirname, 'localGUI', 'style.css'));
+});
+
+// Root route to serve GUI.html
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'localGUI', 'GUI.html'));
+});
+
+
+// Serve static files from localGUI directory
+app.use(express.static(path.join(__dirname, 'webPage')));
 
 app.listen(3000, () => {
 	console.log("Server started on port 3000");
