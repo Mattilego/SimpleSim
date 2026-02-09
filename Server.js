094f849bbe8f80884715962ce12165dfe13f2809
Mattias <mattiasstrandberg4@gmail.com>
2026-02-09 16:09:36 +0100

    relized that the webpage and local gui didn't really do anything different

--- Server.js
+++ Server.js
@@ -2,23 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get('/GUImain.js', (req, res) => {
-	res.sendFile(path.join(__dirname, 'webpage', 'webPageMain.js'));
-});
-
 app.get('/blockly/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'node_modules', 'blockly', req.params[0]));
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'localGUI', 'GUI.html'));
 });
 
 app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'localGUI', req.params[0]));
 });
-
-// Root route to serve GUI.html
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'localGUI', 'GUI.html'));
-});
-
 
 // Serve static files from localGUI directory
 app.use(express.static(path.join(__dirname, 'webPage')));
